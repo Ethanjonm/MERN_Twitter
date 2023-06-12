3f2af20c3e3bf9f7d2a112aa6131fa81c2f5838f
Ethanjonm <ethanjonmercado@gmail.com>
2023-06-11 23:57:44 -0400

    backend tweets

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,7 @@
 
+
 module.exports = {
+    secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
     isProduction: process.env.NODE_ENV === 'production'
-  }
+  };
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,14 +5,29 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
+
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
   res.json({
-    message: "GET /api/users"
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
   });
 });
 
-router.post('/register', async (req, res, next) => {
+router.post('/register', validateRegisterInput, async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
   const user = await User.findOne({
@@ -20,7 +35,7 @@ router.post('/register', async (req, res, next) => {
   });
 
   if (user) {
-    // Throw a 400 error if the email address and/or email already exists
+    // Throw a 400 error if the email address and/or username already exists
     const err = new Error("Validation Error");
     err.statusCode = 400;
     const errors = {};
@@ -47,7 +62,7 @@ router.post('/register', async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
       }
       catch(err) {
         next(err);
@@ -56,7 +71,8 @@ router.post('/register', async (req, res, next) => {
   });
 });
 
-router.post('/login', async (req, res, next) => {
+// POST /api/users/login
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
@@ -65,10 +81,9 @@ router.post('/login', async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
   })(req, res, next);
 });
-
 
 
 
